f552a4623ca542c9a066cc8e9e37512696703c05
Diego <britodiego.tech@gmail.com>
2025-10-23 13:29:31 -0400

    feat(Worker): Implement workers to handle jobs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,71 @@
-use std::thread::JoinHandle;
+use std::sync::{
+    Arc, Mutex,
+    mpsc::{Receiver, Sender, channel},
+};
+use std::thread::{self, JoinHandle};
 
+#[allow(dead_code)]
 pub struct ThreadPool {
-   pub threads: Vec<JoinHandle<()>>
+    workers: Vec<Worker>,
+    sender: Sender<Job>,
 }
 
 #[derive(Debug)]
 pub enum PoolCreateError {
-    NonValueZeroAllowed
+    NonValueZeroAllowed,
 }
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
+    /// Build a ThreadPool.
     ///
     /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreateError> {
-
         if size == 0 {
             return Err(PoolCreateError::NonValueZeroAllowed);
         }
 
-        let mut threads = Vec::with_capacity(size);
+        let (sendx, recx) = channel::<Job>();
+        let receiver_clone = Arc::new(Mutex::new(recx));
 
-        for _ in 0..size {
-            
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver_clone)));
         }
 
         Ok(ThreadPool {
-            threads
+            workers,
+            sender: sendx,
         })
     }
 }
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn execute<F>(&self, f: F) 
-    where 
-        F: FnOnce() + Send + 'static
-     {
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
 
+#[allow(dead_code)]
+struct Worker {
+    id: usize,
+    thread: JoinHandle<Arc<Mutex<Receiver<Job>>>>,
+}
+
+impl Worker {
+    fn new(id: usize, recx: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = recx.lock().unwrap().recv().unwrap();
+                println!("Worker {id} got a job; executing.");
+                job();
+            }
+        });
+        Worker { id: id, thread }
     }
 }
